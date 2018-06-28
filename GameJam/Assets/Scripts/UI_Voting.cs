47b8bcb285e98337c89750e54775fda1ab2b083e
Bassett7 <1basss31@solent.ac.uk>
2018-06-28 13:50:09 +0100

    voting for all 4, arrows disapear on majority

--- GameJam/Assets/Scripts/UI_Voting.cs
+++ GameJam/Assets/Scripts/UI_Voting.cs
@@ -6,75 +6,17 @@ using UnityEngine.UI;
 public class UI_Voting : MonoBehaviour {
 
     public Text Red_Txt, Blue_Txt, Green_Txt, Yellow_Txt;
+    public Text Red_Voted, Blue_Voted, Green_Voted, Yellow_Voted;
     private bool Choice_Red, Choice_Blue, Choice_Green, Choice_Yellow;
     private bool Choice_Red2, Choice_Blue2, Choice_Green2, Choice_Yellow2;
+    private bool Choice_Red3, Choice_Blue3, Choice_Green3, Choice_Yellow3;
+    private bool Choice_Red4, Choice_Blue4, Choice_Green4, Choice_Yellow4;
     public int Red, Blue, Green, Yellow;
     private int Red_t, Blue_t, Yellow_t, Green_t;
     private int Red_t2, Blue_t2, Yellow_t2, Green_t2;
 
+    public GameObject VotingArrows;
 
-    //   // Use this for initialization
-    //   void Start () {
-
-    //}
-
-    //// Update is called once per frame
-    //void Update () {
-    //       VotePressed();
-    //       Vote_Count();
-    //}
-
-    //   void VotePressed()
-    //   {
-    //       if(Choice_Red == false && Input.GetKey("up"))
-    //       {
-    //           Red_t = Red;
-    //           Green = Green_t;
-    //           Yellow = Yellow_t;
-    //           Blue = Blue_t;
-    //           Red += 1;
-    //           Choice_Red = true;
-    //           Choice_Blue = false;
-    //           Choice_Green = false;
-    //           Choice_Yellow = false;
-    //       }
-    //       if (Choice_Green == false && Input.GetKey("down"))
-    //       {
-    //           Green_t = Green;
-    //           Yellow = Yellow_t;
-    //           Blue = Blue_t;
-    //           Red = Red_t;
-    //           Green += 1;
-    //           Choice_Green = true;
-    //           Choice_Blue = false;
-    //           Choice_Yellow = false;
-    //           Choice_Red = false;
-    //       }
-    //       if (Choice_Yellow == false && Input.GetKey("left"))
-    //       {
-    //           Yellow_t = Yellow;
-    //           Green = Green_t;
-    //           Red = Red_t;
-    //           Blue = Blue_t;
-    //           Yellow += 1;
-    //           Choice_Yellow = true;
-    //           Choice_Blue = false;
-    //           Choice_Green = false;
-    //           Choice_Red = false;
-    //       }
-    //       if (Choice_Blue == false && Input.GetKey("right"))
-    //       {
-    //           Blue_t = Blue;
-    //           Green = Green_t;
-    //           Red = Red_t;
-    //           Yellow = Yellow_t;
-    //           Blue += 1;
-    //           Choice_Blue = true;
-    //           Choice_Green = false;
-    //           Choice_Yellow = false;
-    //           Choice_Red = false;
-    //       }
-    //   }
 
     void Vote_Count()
     {
@@ -82,6 +24,42 @@ public class UI_Voting : MonoBehaviour {
         Blue_Txt.text = "" + Blue;
         Green_Txt.text = "" + Green;
         Yellow_Txt.text = "" + Yellow;
+
+        if(Red >= 3)
+        {
+            Red_Voted.gameObject.SetActive(true);
+            Invoke("DisableText", 3f);
+            Red = 0;
+            Yellow = 0;
+            Blue = 0;
+            Green = 0;
+            
+            //StartCoroutine(TextWait());
+            //Red_Voted.gameObject.SetActive(false);
+            //Set players percentage to 999 here
+        }
+
+        else if (Yellow >= 3)
+        {
+            Yellow_Voted.gameObject.SetActive(true);
+           // StartCoroutine(VoteWait());
+           // Yellow_Voted.gameObject.SetActive(false);
+            //set players percentage to 999 here
+        }
+        else if (Green >= 3)
+        {
+            Green_Voted.gameObject.SetActive(true);
+           // StartCoroutine(VoteWait());
+           // Green_Voted.gameObject.SetActive(false);
+            //set players percentage to 999 here
+        }
+        else if (Blue >= 3)
+        {
+            Blue_Voted.gameObject.SetActive(true);
+           // StartCoroutine(VoteWait());
+           // Blue_Voted.gameObject.SetActive(false);
+            //set players percentage to 999 here
+        }
     }
 
 
@@ -91,12 +69,27 @@ public class UI_Voting : MonoBehaviour {
     public string player2AxisVertical;
     public string player2AxisHorizontal;
 
+    public string player3AxisVertical;
+    public string player3AxisHorizontal;
+
+    public string player4AxisVertical;
+    public string player4AxisHorizontal;
+
     private bool p1votedR, p1votedG, p1votedB, p1votedY;
     private bool p2votedR, p2votedG, p2votedB, p2votedY;
+    private bool p3votedR, p3votedG, p3votedB, p3votedY;
+    private bool p4votedR, p4votedG, p4votedB, p4votedY;
+
+    bool WaitCompleted;
+
+    private void Start()
+    {
+        WaitCompleted = true;
+    }
 
     private void Update()
     {
-        if(Choice_Blue == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisHorizontal) == 1)
+        if(WaitCompleted == true && Choice_Blue == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisHorizontal) == 1)
         {
             Debug.Log("player 1 right");
 
@@ -125,8 +118,10 @@ public class UI_Voting : MonoBehaviour {
             p1votedY = false;
             p1votedG = false;
 
+            StartCoroutine(VoteWait());
+
         }
-        else if(Choice_Yellow == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisHorizontal) == -1)
+        else if(WaitCompleted == true && Choice_Yellow == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisHorizontal) == -1)
         {
             Debug.Log("player 1 left");
 
@@ -151,8 +146,10 @@ public class UI_Voting : MonoBehaviour {
             p1votedG = false;
             p1votedR = false;
             p1votedB = false;
+
+            StartCoroutine(VoteWait());
         }
-        else if (Choice_Red == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisVertical) == 1)
+        else if (WaitCompleted == true && Choice_Red == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisVertical) == 1)
         {
             Debug.Log("player 1 up");
 
@@ -176,8 +173,10 @@ public class UI_Voting : MonoBehaviour {
             p1votedB = false;
             p1votedG = false;
             p1votedY = false;
+
+            StartCoroutine(VoteWait());
         }
-        else if (Choice_Green == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisVertical) == -1)
+        else if (WaitCompleted == true && Choice_Green == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player1AxisVertical) == -1)
         {
             Debug.Log("player 1 down");
 
@@ -201,11 +200,13 @@ public class UI_Voting : MonoBehaviour {
             p1votedY = false;
             p1votedB = false;
             p1votedR = false;
+
+            StartCoroutine(VoteWait());
         }
 
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////// Player 2 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        else if (Choice_Blue2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisHorizontal) == 1)
+        else if (WaitCompleted == true && Choice_Blue2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisHorizontal) == 1)
         {
             Debug.Log("player 2 right");
 
@@ -229,8 +230,10 @@ public class UI_Voting : MonoBehaviour {
             p2votedR = false;
             p2votedY = false;
             p2votedG = false;
+
+            StartCoroutine(VoteWait());
         }
-        else if (Choice_Yellow2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisHorizontal) == -1)
+        else if (WaitCompleted == true && Choice_Yellow2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisHorizontal) == -1)
         {
             Debug.Log("player 2 left");
 
@@ -255,8 +258,10 @@ public class UI_Voting : MonoBehaviour {
             p2votedG = false;
             p2votedR = false;
             p2votedB = false;
+
+            StartCoroutine(VoteWait());
         }
-        else if (Choice_Red2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisVertical) == 1)
+        else if (WaitCompleted == true && Choice_Red2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisVertical) == 1)
         {
             Debug.Log("player 2 up");
 
@@ -280,9 +285,11 @@ public class UI_Voting : MonoBehaviour {
             p2votedB = false;
             p2votedG = false;
             p2votedY = false;
+
+            StartCoroutine(VoteWait());
         }
 
-        else if (Choice_Green2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisVertical) == -1)
+        else if (WaitCompleted == true && Choice_Green2 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player2AxisVertical) == -1)
         {
             Debug.Log("player 2 down");
 
@@ -306,9 +313,251 @@ public class UI_Voting : MonoBehaviour {
             p2votedY = false;
             p2votedB = false;
             p2votedR = false;
+
+            StartCoroutine(VoteWait());
+        }
+
+        /////////////////////////////////////////////////////////////////////// Player 3 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        else if (WaitCompleted == true && Choice_Blue3 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player3AxisHorizontal) == 1)
+        {
+            Debug.Log("player 3 right");
+
+            if (p3votedR == true)
+                Red -= 1;
+
+            if (p3votedG == true)
+                Green -= 1;
+
+            if (p3votedY == true)
+                Yellow -= 1;
+
+            Blue += 1;
+            p3votedB = true;
+
+            Choice_Blue3 = true;
+            Choice_Green3 = false;
+            Choice_Yellow3 = false;
+            Choice_Red3 = false;
+
+            p3votedR = false;
+            p3votedY = false;
+            p3votedG = false;
+
+            StartCoroutine(VoteWait());
+        }
+        else if (WaitCompleted == true && Choice_Yellow3 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player3AxisHorizontal) == -1)
+        {
+            Debug.Log("player 3 left");
+
+            if (p3votedB == true)
+                Blue -= 1;
+
+            if (p3votedG == true)
+                Green -= 1;
+
+            if (p3votedR == true)
+                Red -= 1;
+
+            Yellow += 1;
+            p3votedY = true;
+
+
+            Choice_Yellow3 = true;
+            Choice_Blue3 = false;
+            Choice_Green3 = false;
+            Choice_Red3 = false;
+
+            p3votedG = false;
+            p3votedR = false;
+            p3votedB = false;
+
+            StartCoroutine(VoteWait());
+        }
+        else if (WaitCompleted == true && Choice_Red3 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player3AxisVertical) == 1)
+        {
+            Debug.Log("player 3 up");
+
+            if (p3votedB == true)
+                Blue -= 1;
+
+            if (p3votedG == true)
+                Green -= 1;
+
+            if (p3votedY == true)
+                Yellow -= 1;
+
+            Red += 1;
+            p3votedR = true;
+
+            Choice_Red3 = true;
+            Choice_Blue3 = false;
+            Choice_Green3 = false;
+            Choice_Yellow3 = false;
+
+            p3votedB = false;
+            p3votedG = false;
+            p3votedY = false;
+
+            StartCoroutine(VoteWait());
+        }
+
+        else if (WaitCompleted == true && Choice_Green3 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player3AxisVertical) == -1)
+        {
+            Debug.Log("player 3 down");
+
+            if (p3votedB == true)
+                Blue -= 1;
+
+            if (p3votedY == true)
+                Yellow -= 1;
+
+            if (p3votedR == true)
+                Red -= 1;
+
+            Green += 1;
+            p3votedG = true;
+
+            Choice_Green3 = true;
+            Choice_Blue3 = false;
+            Choice_Yellow3 = false;
+            Choice_Red3 = false;
+
+            p3votedY = false;
+            p3votedB = false;
+            p3votedR = false;
+
+            StartCoroutine(VoteWait());
+        }
+        /////////////////////////////////////////////////////////////////////// Player 4 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        else if (WaitCompleted == true && Choice_Blue4 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player4AxisHorizontal) == 1)
+        {
+            Debug.Log("player 4 right");
+
+            if (p4votedR == true)
+                Red -= 1;
+
+            if (p4votedG == true)
+                Green -= 1;
+
+            if (p4votedY == true)
+                Yellow -= 1;
+
+            Blue += 1;
+            p4votedB = true;
+
+            Choice_Blue4 = true;
+            Choice_Green4 = false;
+            Choice_Yellow4 = false;
+            Choice_Red4 = false;
+
+            p4votedR = false;
+            p4votedY = false;
+            p4votedG = false;
+
+            StartCoroutine(VoteWait());
+        }
+        else if (WaitCompleted == true && Choice_Yellow4 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player4AxisHorizontal) == -1)
+        {
+            Debug.Log("player 4 left");
+
+            if (p4votedB == true)
+                Blue -= 1;
+
+            if (p4votedG == true)
+                Green -= 1;
+
+            if (p4votedR == true)
+                Red -= 1;
+
+            Yellow += 1;
+            p4votedY = true;
+
+
+            Choice_Yellow4 = true;
+            Choice_Blue4 = false;
+            Choice_Green4 = false;
+            Choice_Red4 = false;
+
+            p4votedG = false;
+            p4votedR = false;
+            p4votedB = false;
+
+            StartCoroutine(VoteWait());
+        }
+        else if (WaitCompleted == true && Choice_Red4 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player4AxisVertical) == 1)
+        {
+            Debug.Log("player 4 up");
+
+            if (p4votedB == true)
+                Blue -= 1;
+
+            if (p4votedG == true)
+                Green -= 1;
+
+            if (p4votedY == true)
+                Yellow -= 1;
+
+            Red += 1;
+            p4votedR = true;
+
+            Choice_Red4 = true;
+            Choice_Blue4 = false;
+            Choice_Green4 = false;
+            Choice_Yellow4 = false;
+
+            p4votedB = false;
+            p4votedG = false;
+            p4votedY = false;
+
+            StartCoroutine(VoteWait());
+        }
+
+        else if (WaitCompleted == true && Choice_Green4 == false && UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxisRaw(player4AxisVertical) == -1)
+        {
+            Debug.Log("player 4 down");
+
+            if (p4votedB == true)
+                Blue -= 1;
+
+            if (p4votedY == true)
+                Yellow -= 1;
+
+            if (p4votedR == true)
+                Red -= 1;
+
+            Green += 1;
+            p4votedG = true;
+
+            Choice_Green4 = true;
+            Choice_Blue4 = false;
+            Choice_Yellow4 = false;
+            Choice_Red4 = false;
+
+            p4votedY = false;
+            p4votedB = false;
+            p4votedR = false;
+
+            StartCoroutine(VoteWait());
         }
 
         Vote_Count();
 
     }
+
+    IEnumerator VoteWait()
+    {
+        WaitCompleted = false;
+        yield return new WaitForSeconds(2);
+        WaitCompleted = true;
+    }
+
+    void DisableText()
+    {
+        Red_Voted.gameObject.SetActive(false);
+        VotingArrows.gameObject.SetActive(false);
+    }
+
+
 }
