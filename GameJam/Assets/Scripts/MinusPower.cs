c23b4bf0e5a3b62be2a72c9c16cfc9a2b9ec099d
1kellj88 <1kellj88@solent.ac.uk>
2018-06-29 01:03:17 +0100

    PowerUps Done, Simple Animations Done, UI Changed

---
+++ GameJam/Assets/Scripts/MinusPower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinusPower : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            GameController.powerCharge = GameController.powerCharge - 10;
+        }
+
+        if (collision.gameObject.tag == "Death")
+        {
+            Destroy(this.gameObject);
+            
+        }
+    }
+
+
+}
